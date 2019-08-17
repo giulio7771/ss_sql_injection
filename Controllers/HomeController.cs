6cd8c8bad3416043594ed686412867d48fb68089
matmahnke <matmahnke@gmail.com>
2019-08-17 02:31:50 -0300

    git ignore

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using projeto_mvc.Models;
-using System.Data;
-using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using projeto_mvc.Services;
+using projeto_mvc.Models;
 
 namespace projeto_mvc.Controllers
 {
@@ -14,24 +16,17 @@ namespace projeto_mvc.Controllers
     {
         public IActionResult Index()
         {
-          string connectionString = "Server=172.17.0.2;Database=exe1;Uid=root;Pwd=root;";
-          using (SqlConnection connection = new SqlConnection(connectionString))
-          {
-            string sql = $"QUERY";
-		        using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                command.CommandType = CommandType.Text;
-                connection.Open();
-                command.ExecuteNonQuery();
-                connection.Close();
-            }
-        }
-        return View();
-  }
-
-        public IActionResult Privacy()
-        {
             return View();
+        }
+
+        public IActionResult visualizar(){
+            List<UsuarioViewModel> result;
+
+                using(var context = new DbContext<UsuarioViewModel>())
+                {
+                    result = context.Select("SELECT * FROM USUARIOS");
+                }
+            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
